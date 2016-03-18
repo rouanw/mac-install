4bc5bf22610945722a5f5705719468d8d79276b9
Rouan <rmw@thoughtworks.com>
2016-03-18 22:29:41 +0200

    initial ínstall script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+brew update
+brew doctor
+
+brew install git
+brew install node
+brew install cdargs
+brew install gpg2
+
+brew tap caskroom/cask
+brew cask install google-chrome
+brew cask install dockertoolbox
+brew cask install alfred
+brew cask install 1password
+brew cask install atom
+brew cask install caffeine
+brew cask install scroll-reverser
+brew cask install vlc
+brew cask install adium
+brew cask install dropbox
+brew cask install eloquent
+brew cask install firefox
+brew cask install google-drive
+brew cask install menucalendarclock-ical
+brew cask install skype
